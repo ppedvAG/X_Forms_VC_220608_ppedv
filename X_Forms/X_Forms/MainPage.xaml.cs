dbca3350b4483242800c4dfd23f83ac23396d47b
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-06-08 13:19:56 +0200

    Add project files.

---
+++ X_Forms/X_Forms/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace X_Forms
+{
+    public partial class Hauptseite : ContentPage
+    {
+        public Hauptseite()
+        {
+            this.Resources["BtnString"] = "Ich bin eine neue Resource";
+            InitializeComponent();
+
+        }
+
+        private void Btn_KlickMich_Clicked(object sender, EventArgs e)
+        {
+            Lbl_Output.Text = "Button wurde geklickt";
+
+            if (sender is Button)
+                (sender as Button).BackgroundColor = Color.HotPink;
+
+            Ent_Input.Text = Sdr_Wert.Value.ToString();
+
+            Lbl_Output.Text = Pkr_Namen.SelectedItem?.ToString();
+        }
+
+        private void Ent_Input_Completed(object sender, EventArgs e)
+        {
+            Lbl_Output.Text = Ent_Input.Text;
+        }
+
+        private void Sdr_Wert_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Sdr_Wert.Value = Math.Round(Sdr_Wert.Value);
+        }
+    }
+}
